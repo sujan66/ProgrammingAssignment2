55614d9aa86c986c77a6ac0ff22c80c895917d04
sujan66 <sujanveena66@gmail.com>
2020-09-03 20:57:50 +0530

    Initial un-tested code for cached Inverse program

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set_matrix <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get_matrix <- function() x
+  set_inverse <- function(inverse_t) inverse <<- inverse_t
+  get_inverse <- function() inverse
+  list(setMatrix = set_matrix, getMatrix = get_matrix, setInverse = set_inverse, getInverse = get_inverse)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if (!is.null(inverse)) {
+    message("Returning cached data")
+    return(inverse)
+  }
+  matrix_data <- x$getMatrix()
+  inverse <- solve(matrix_data)
+  x$setInverse(inverse)
+  inverse
 }
